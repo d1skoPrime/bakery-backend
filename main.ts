49fc8efb6b6c082c52b1ce04af44286cef6d1ea9
d1sko1459@gmail.com <d1sko1459@gmail.com>
2025-03-09 18:42:14 -0700

    init

---
+++ main.ts
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/send-email", async (req:any, res:any) => {
+  const { firstName, lastName, email, message, phonenumber, productname } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER, // Твой email
+      pass: process.env.EMAIL_PASS, // Пароль или App Password
+    },
+  });
+	const emailContent = `
+    <h1>Новый Заказ От ${firstName} ${lastName}</h1>
+    <p><strong>Полное Имя Клиента:</strong> ${firstName} ${lastName}</p>
+    <p><strong>Почта Клиента:</strong> ${email}</p>
+    <p><strong>Номер Телефона Клиента: ${phonenumber}</p>
+    <p><strong>Название Продукта:</strong> ${productname}</p>
+    <p><strong>Доп.Сообщение от клиента:</strong></p>
+    <p>${message}</p>
+  `;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER, // Самому себе
+    subject: `Новый заказ от ${firstName}`,
+    html:emailContent,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to send email" });
+  }
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
